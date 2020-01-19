db175ec87a7f3149f1fce68dd42c7782b2e6387e
brunorech <brunogrech@hotmail.com>
2020-01-19 16:37:20 -0300

    Adicionado sistema de loading no formulario

--- src/assets/styles/S.HomePage.js
+++ src/assets/styles/S.HomePage.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import background from '../images/background2.jpg';
+import background from '../images/background.jpg';
 
 const Container = styled.div`
     width: 80%;
--- src/components/ClientForm.js
+++ src/components/ClientForm.js
@@ -7,6 +7,7 @@ export default ({ client, SetFormOpen }) => {
 
     const [clientData, setClientData] = useState({ ...client });
     const [cities, setCities] = useState([]);
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchCities = async () => {
@@ -20,6 +21,7 @@ export default ({ client, SetFormOpen }) => {
 
     const handleSave = async () => {
         let response;
+        setLoading(true);
         if (client) {
             response = await api.put(`/clientes/${client.id}`, { ...clientData });
         } else {
@@ -27,15 +29,21 @@ export default ({ client, SetFormOpen }) => {
         }
         const { error } = response.data;
         if (error) {
+            setLoading(false);
             return toast.error(error);
         }
+        setLoading(false);
+        SetFormOpen(false);
         return toast.success('Cliente salvo com sucesso');
 
     };
 
     const handleDelete = async () => {
+        setLoading(true);
         if (client) {
             await api.delete(`/clientes/${client.id}`);
+            setLoading(false);
+            SetFormOpen(false);
             return toast.success('Cliente deletado com sucesso');
         }
     }
@@ -52,47 +60,54 @@ export default ({ client, SetFormOpen }) => {
             <Container>
                 <BackButton
                     onClick={() => SetFormOpen(false)}
-                >
+                    disabled={isLoading}
+                    >
                     Voltar
             </BackButton>
-                <InputLabel>Nome</InputLabel>
-                <ClientInput
+                    <InputLabel>Nome</InputLabel>
+                    <ClientInput
+                    disabled={isLoading}
                     onChange={handleChange}
                     value={clientData ? clientData.nome : ''}
                     name="nome"
                     type="text"
-                />
-                <InputLabel>Endereco</InputLabel>
-                <ClientInput
+                    />
+                    <InputLabel>Endereco</InputLabel>
+                    <ClientInput
+                    disabled={isLoading}
                     onChange={handleChange}
                     value={clientData ? clientData.endereco : ''}
                     name="endereco"
                     type="text"
-                />
-                <InputLabel>Cnpj</InputLabel>
-                <ClientInput
+                    />
+                    <InputLabel>Cnpj</InputLabel>
+                    <ClientInput
+                    disabled={isLoading}
                     onChange={handleChange}
                     value={clientData ? clientData.cnpj : ''}
                     name="cnpj"
                     type="text"
-                />
-                <InputLabel>Municipio</InputLabel>
-                <CityCombobox
-                    onChange={handleChange}
-                    name="municipio_id"
-                    value={clientData.municipio_id ? clientData.municipio_id : ''}
-                >
-                    {cities.map(city => (
-                        <CityOption key={city.id} value={city.id}>{city.nome} - {city.estado}</CityOption>
-                    ))}
-                </CityCombobox>
+                    />
+                    <InputLabel>Municipio</InputLabel>
+                    <CityCombobox
+                        disabled={isLoading}
+                        onChange={handleChange}
+                        name="municipio_id"
+                        value={clientData.municipio_id ? clientData.municipio_id : ''}
+                    >
+                        {cities.map(city => (
+                            <CityOption key={city.id} value={city.id}>{city.nome} - {city.estado}</CityOption>
+                        ))}
+                    </CityCombobox>
                 <DeleteButton
                     onClick={handleDelete}
+                    disabled={isLoading}
                 >
                     Excluir
                 </DeleteButton>
                 <SaveButton
                     onClick={handleSave}
+                    disabled={isLoading}
                 >
                     Salvar
                 </SaveButton>
